12725fa0fbdbe629984d804e8a2f2023defec943
Taha_Arshad <ta.arshad786@gmail.com>
2023-12-31 10:54:52 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Photo from "./components/Photo";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import 'lightbox.js-react/dist/index.css';
 
 const App = () => {
   return (
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -7,56 +7,67 @@ import image5 from "../assets/images/1 (5).jpg";
 import image6 from "../assets/images/1 (6).jpg";
 import image7 from "../assets/images/1 (7).jpg";
 import image8 from "../assets/images/1 (8).jpg";
-import "./Photo.css"
+import "./Photo.css";
+import LightGallery from "lightgallery/react";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
 
 const Photo = () => {
+  const onBeforeSlide = (detail) => {
+    const { index, prevIndex } = detail;
+    console.log(index, prevIndex);
+  };
+  const onInit = () => {
+    console.log("lightGallery has been initialized");
+  };
   return (
     <>
-      <section class="gallery min-vh-100">
-        <h1 class="text-center mt-5 mb-3">Photo Gallery</h1>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-2">
-          <div class="col">
-            <a href={image1} data-lightbox="models">
-              <img src={image1} class="gallery-item" alt="gallery" />
+      <h1 className="text-center mt-5 mb-3">Photo Gallery</h1>
+      <div className="gallery">
+        <div className="main">
+          <LightGallery
+            onInit={onInit}
+            speed={500}
+            onBeforeSlide={onBeforeSlide}
+            plugins={[lgThumbnail, lgZoom]}
+          >
+            <a href={image1} className="col">
+              <img src={image1} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image2} data-lightbox="models">
-              <img src={image2} class="gallery-item" alt="gallery" />
+
+            <a href={image2} className="col">
+              <img src={image2} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image3} data-lightbox="models">
-              <img src={image3} class="gallery-item" alt="gallery" />
+
+            <a href={image3} className="col">
+              <img src={image3} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image4} data-lightbox="models">
-              <img src={image4} class="gallery-item" alt="gallery" />
+
+            <a href={image4} className="col">
+              <img src={image4} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image5} data-lightbox="models">
-              <img src={image5} class="gallery-item" alt="gallery" />
+
+            <a href={image5} className="col">
+              <img src={image5} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image6} data-lightbox="models">
-              <img src={image6} class="gallery-item" alt="gallery" />
+
+            <a href={image6} className="col">
+              <img src={image6} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image7} data-lightbox="models">
-              <img src={image7} class="gallery-item" alt="gallery" />
+
+            <a href={image7} className="col">
+              <img src={image7} className="gallery-item" alt="gallery" />
             </a>
-          </div>
-          <div class="col">
-            <a href={image8} data-lightbox="models">
-              <img src={image8} class="gallery-item" alt="gallery" />
+
+            <a href={image8} className="col">
+              <img src={image8} className="gallery-item" alt="gallery" />
             </a>
-          </div>
+          </LightGallery>
         </div>
-      </section>
+      </div>
     </>
   );
 };
